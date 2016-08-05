4fe63b48aa6a1d673048e4c23daedaef0fd15a3d
todaylg <2489437918@qq.com>
2016-08-05 22:14:46 +0800

    路由的初始化，以及各个service的init调用

--- LG/server/routes/routes.js
+++ LG/server/routes/routes.js
@@ -11,7 +11,7 @@ module.exports = function(app){
       throw '没有找到该文件夹，请检查......'
     }
     //路由只管分配，不管controller
-    for (var e; list.length && (e = list.shift());){//shift() 方法用于把数组的第一个元素从其中删除，并返回第一个元素的值。
+    for (var e; list.length && (e = list.shift());){//shift() 方法用于把数组的第一个元素从其中删除，并返回第一个元素的值。就是一个一个来啦
       var service = require(REQUIRE_PATH_SERVICES + e);
       service.init && service.init(app);
     }
